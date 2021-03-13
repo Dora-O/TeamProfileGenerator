dc485fd963274b80774a569b597d1515fcd2bbb3
Dora O <dorozco2390@utexas.edu>
2021-03-12 20:55:49 -0600

    updated questions for all three roles

--- index.js
+++ index.js
@@ -7,25 +7,67 @@ const managerQuestions = [
     {
         type: "input",
         message: "What is the team manager's name?",
-        name: "TMname"
-        },
+        name: "name"
+    },
     {
         type: "input",
         message: "What is the team manager's id?",
-        name: "TMid"
-        },
+        name: "id"
+    },
     {
         type: "input",
         message: "What is the team manager's email?",
-        name: "TMemail"
-        },
+        name: "email"
+    },
     {
         type: "input",
         message: "What is the team manager's office number?",
-        name: "TMnumber"
-        },
+        name: "officeNumber"
+    },
 ];
 
-const engineerQuestions = [];
+const engineerQuestions = [
+    {
+        type: "input",
+        message: "What is your engineer's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your engineer's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is your engineer's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your engineer's GitHub username?",
+        name: "github"
+    },
+];
 
-const internQuestions = [];
+const internQuestions = [
+    {
+        type: "input",
+        message: "What is your intern's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your intern's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is your intern's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What univeristy does your intern attend?",
+        name: "github"
+    },
+];
