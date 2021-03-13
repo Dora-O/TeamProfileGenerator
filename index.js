e1dcb5592de2faeb19b44edc04d7a91c00978599
Dora O <dorozco2390@utexas.edu>
2021-03-12 20:51:14 -0600

    changed inquirer promot to const questions for all three job roles

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: "input",
-            message: "What is the team manager's name?",
-            name: "TMname"
-        },
-        {
-            type: "input",
-            message: "What is the team manager's id?",
-            name: "TMid"
-        },
-        {
-            type: "input",
-            message: "What is the team manager's email?",
-            name: "TMemail"
-        },
-        {
-            type: "input",
-            message: "What is the team manager's office number?",
-            name: "TMnumber"
-        },
+const teamMembers = [];
 
-    ])
-}
+const managerQuestions = [
+    {
+        type: "input",
+        message: "What is the team manager's name?",
+        name: "TMname"
+        },
+    {
+        type: "input",
+        message: "What is the team manager's id?",
+        name: "TMid"
+        },
+    {
+        type: "input",
+        message: "What is the team manager's email?",
+        name: "TMemail"
+        },
+    {
+        type: "input",
+        message: "What is the team manager's office number?",
+        name: "TMnumber"
+        },
+];
+
+const engineerQuestions = [];
+
+const internQuestions = [];
