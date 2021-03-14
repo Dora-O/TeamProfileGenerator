4eb9f066dd28e355166097547e398662d60a1d44
Dora O <dorozco2390@utexas.edu>
2021-03-13 22:04:28 -0600

    created test js and verified all test passed

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,18 @@
+const Employee = require("../lib/Employee");
+const Engineer = require('../lib/Engineer');
+
+describe("getRole", () => {
+    it("Returns role", () => {
+        const role = "Engineer";
+        const data = new Engineer('Jarvis', 2, 'jarvis@mail.com');
+        expect(data.getRole()).toBe(role);
+    });
+});
+
+describe("getGithub", () => {
+    it("Returns github", () => {
+        const gitHub = "Githubuser";
+        const data = new Engineer('Jarvis', 2, 'jarvis@mail.com');
+        expect(data.getGithub()).toBe(gitHub);
+    });
+});
